05ba6ae4cfe6637d3a187e46adc844999a0110d7
Александр Захаров <zaher0861@yandex.ru>
2022-02-15 21:21:02 +0300

    Добавьте файлы проекта.

---
+++ LessonWPF_6/WeatherControl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace LessonWPF_6
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TemperatureProperty;
+        public string DirectionOfTheWInd;
+        public int SpeedOfTheWind;
+        public enum Precipitation
+        {
+            Sunny,
+            Cloudy,
+            Rain,
+            Snow
+        }
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+        public WeatherControl(string direction, int speed, Precipitation precipitation, int temp)
+        {
+            this.DirectionOfTheWInd = direction;
+            this.SpeedOfTheWind = speed;
+            this.Temperature = temp;
+        }
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                null,
+                new CoerceValueCallback(CoerceTemperature)),
+                new ValidateValueCallback(ValidateTemperature));
+        }
+
+        private static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+            {
+                return v;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
